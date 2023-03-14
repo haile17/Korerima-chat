edbb36c7b1d3418fa840a5587e2f308e941949e5
Haile Fikere <hailefikere17@gmail.com>
2023-03-14 15:48:45 +0300

    update

--- src/firebase.js
+++ src/firebase.js
@@ -2,23 +2,20 @@ import { initializeApp } from "firebase/app";
 import { getAuth} from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBzzyC59zD7pn-sC1pKKo9D1LEXK05SVXk",
-  authDomain: "korerima-5b8b6.firebaseapp.com",
-  projectId: "korerima-5b8b6",
-  storageBucket: "korerima-5b8b6.appspot.com",
-  messagingSenderId: "750989944472",
-  appId: "1:750989944472:web:2f6dd6787c3f4219a392ed",
-  measurementId: "G-QQ21BZ8FKJ"
+  apiKey: "AIzaSyBYDxtcnB_s5odMdfkO4B6rxsBfB8x27f4",
+  authDomain: "korerima-chat-4c24c.firebaseapp.com",
+  projectId: "korerima-chat-4c24c",
+  storageBucket: "korerima-chat-4c24c.appspot.com",
+  messagingSenderId: "396017981530",
+  appId: "1:396017981530:web:3657c96bcf15a1cd6ae063"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
 export const auth = getAuth();
 export const storage = getStorage();
 export const db = getFirestore();
